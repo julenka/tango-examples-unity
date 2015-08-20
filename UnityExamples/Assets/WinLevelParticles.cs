2e931d19fc7f0c6f808682eb1bb76ae060dd2342
Julia Schwarz <julenka@cs.cmu.edu>
2015-08-20 16:59:16 -0400

    exit game when you enter winning particle system

---
+++ UnityExamples/Assets/WinLevelParticles.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinLevelParticles : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other)
+    {
+        Application.Quit();
+    }
+}
