0e2fe8f1868d0921edeed182068f1592aa04c0cc
Julia Schwarz <julenka@cs.cmu.edu>
2015-08-20 16:10:54 -0400

    adding turrets. Turrets shoot lasers

---
+++ UnityExamples/Assets/LaserBeam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserBeam : MonoBehaviour
+{
+    public GameObject m_Emitter;
+
+    private LineRenderer m_lineRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_lineRenderer = GetComponent<LineRenderer>();
+        _UpdateLaser();
+    }
+
+    private void _UpdateLaser()
+    {
+        m_lineRenderer.SetPosition(0, m_Emitter.transform.position);
+        RaycastHit raycastHit;
+        if (Physics.Raycast(m_Emitter.transform.position, m_Emitter.transform.forward, out raycastHit))
+        {
+            m_lineRenderer.SetPosition(1, raycastHit.point);
+        }
+        else
+        {
+            Ray unboundedRay = new Ray(m_Emitter.transform.position, m_Emitter.transform.forward);
+            m_lineRenderer.SetPosition(1, unboundedRay.GetPoint(100));
+        }
+        // Cast the ray out until you hit a wall
+        // Get the position of that wall
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
